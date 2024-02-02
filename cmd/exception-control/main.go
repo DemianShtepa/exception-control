736195f6a83c11ddd88348600d235f5eaa18defb
Demian <demyan.shtepa@gmail.com>
2024-02-02 02:06:18 +0200

    Setup configuration

---
+++ cmd/exception-control/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/DemianShtepa/exception-control/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	logger := setupLogger(cfg.Env)
+
+	logger.Info("starting application", slog.String("env", cfg.Env))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	var level slog.Level
+
+	switch env {
+	case envLocal, envDev:
+		level = slog.LevelDebug
+	case envProd:
+		level = slog.LevelInfo
+	default:
+		panic("unsupported env provided")
+	}
+
+	log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level}))
+
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+)
+
+type DBConfig struct {
+	Connection string `yaml:"connection" env:"DB_CONNECTION" env-required:"true"`
+	User       string `yaml:"user" env:"DB_USER" env-required:"true"`
+	Password   string `yaml:"password" env:"DB_PASSWORD" env-required:"true"`
+	Database   string `yaml:"database" env:"DB_DATABASE" env-required:"true"`
+}
+
+type Config struct {
+	Env string   `yaml:"env" env:"ENV" env-default:"local"`
+	DB  DBConfig `yaml:"db"`
+}
+
+func MustLoad() *Config {
+	configPath := resolveConfigPath()
+	if configPath == "" {
+		panic("unable to resolve config path")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("failed to parse config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func resolveConfigPath() string {
+	var path string
+
+	flag.StringVar(&path, "config path", "", "path to config file")
+	flag.Parse()
+
+	if path == "" {
+		path = os.Getenv("CONFIG_PATH")
+	}
+
+	return path
+}
